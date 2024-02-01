c1814e112d85d4a5f4f7b2080904b85d27f80009
agstefana <158216761+agstefana@users.noreply.github.com>
2024-02-01 20:25:47 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
-provider "azurerm" { version = "=1.27.0" }
+provider "azurerm" { 
+    version = "=1.27.0" 
+    
+    subscription_id = "a4bac015-e4c3-49b5-a817-ce49c7cbaa5a"
+    }
 
 resource "azurerm_resource_group" "adrons_resource_group_workspace" {
   name     = "adrons_workspace"
