1e50a8757437ed5167383141d249fdd78b1cf1ef
agstefana <158216761+agstefana@users.noreply.github.com>
2024-02-01 20:17:40 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
 provider "azurerm" { version = "=1.27.0" }
+
+resource "azurerm_resource_group" "adrons_resource_group_workspace" {
+  name     = "adrons_workspace"
+  location = "West US 2"
+
+  tags = {
+    environment = "Development"
+  }
+}
