f1e21ed234028d471872bfcf84f70156d7b34f9e
agstefana <158216761+agstefana@users.noreply.github.com>
2025-11-12 18:33:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-
-
-# main.tf (vulnerable snip) -- fictitious file for exercise
-
 terraform {
   required_providers {
     azurerm = {
@@ -17,98 +13,154 @@ provider "azurerm" {
   features {}
 }
 
+# -----------------------
+# Resource Group
+# -----------------------
 resource "azurerm_resource_group" "rg" {
-name = "rg-demo-securedata"
-location = "Central US"
+  name     = "rg-demo-securedata"
+  location = "Central US"
 }
+
+# -----------------------
+# Network Security Group
+# -----------------------
 resource "azurerm_network_security_group" "nsg" {
-name = "nsg-allow-all-management"
-location = azurerm_resource_group.rg.location
-resource_group_name = azurerm_resource_group.rg.name
-security_rule {
-name = "Allow-SSH-RDP-All"
-priority = 100
-direction = "Inbound"
-access = "Allow"
-protocol = "*"
-source_port_range = "*"
-destination_port_range = "22-3389"
-source_address_prefix = "0.0.0.0/0"
-destination_address_prefix = "*"
+  name                = "nsg-allow-all-management"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH-RDP-All"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "22-3389"
+    source_address_prefix      = "0.0.0.0/0"
+    destination_address_prefix = "*"
+  }
 }
-}
+
+# -----------------------
+# Virtual Network and Subnet
+# -----------------------
 resource "azurerm_virtual_network" "vnet" {
-name = "vnet-demo"
-address_space = ["10.0.0.0/16"]
-location = azurerm_resource_group.rg.location
-resource_group_name = azurerm_resource_group.rg.name
+  name                = "vnet-demo"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
+
 resource "azurerm_subnet" "subnet" {
-name = "subnet-demo"
-resource_group_name = azurerm_resource_group.rg.name
-virtual_network_name = azurerm_virtual_network.vnet.name
-address_prefixes = ["10.0.1.0/24"]
-network_security_group_id = azurerm_network_security_group.nsg.id
+  name                 = "subnet-demo"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
-resource "azurerm_network_interface" "nic" {
-name = "nic-vm001"
-location = azurerm_resource_group.rg.location
-resource_group_name = azurerm_resource_group.rg.name
-ip_configuration {
-name = "ipconfig1"
-subnet_id = azurerm_subnet.subnet.id
-private_ip_address_allocation = "Dynamic"
-public_ip_address_id = azurerm_public_ip.vm_pip.id
+
+# Associate NSG to Subnet (new syntax)
+resource "azurerm_subnet_network_security_group_association" "assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
-}
+
+# -----------------------
+# Public IP
+# -----------------------
 resource "azurerm_public_ip" "vm_pip" {
-name = "pip-vm001"
-location = azurerm_resource_group.rg.location
-resource_group_name = azurerm_resource_group.rg.name
-allocation_method = "Static"
+  name                = "pip-vm001"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Basic"
 }
+
+# -----------------------
+# Network Interface
+# -----------------------
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-vm001"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_pip.id
+  }
+}
+
+# -----------------------
+# Linux Virtual Machine
+# -----------------------
 resource "azurerm_linux_virtual_machine" "vm" {
-name = "vm-demo-app"
-resource_group_name = azurerm_resource_group.rg.name
-location = azurerm_resource_group.rg.location
-size = "Standard_B1ms"
-admin_username = "devadmin"
-network_interface_ids = [azurerm_network_interface.nic.id]
-admin_password = "P@ssw0rdDev!!!"
-os_disk {
-caching = "ReadWrite"
-storage_account_type = "Standard_LRS"
+  name                = "vm-demo-app"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1ms"
+  admin_username      = "devadmin"
+  admin_password      = "P@ssw0rdDev!!!"
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
 }
-source_image_reference {
-publisher = "Canonical"
-offer = "UbuntuServer"
-sku = "18.04-LTS"
-version = "latest"
+
+# -----------------------
+# Azure SQL Server + DB (modern provider)
+# -----------------------
+resource "azurerm_mssql_server" "sqlsrv" {
+  name                         = "sql-demo-server1234"
+  resource_group_name           = azurerm_resource_group.rg.name
+  location                      = azurerm_resource_group.rg.location
+  version                       = "12.0"
+  administrator_login           = "sqladmin"
+  administrator_login_password  = "SqlAdminPwd!!"
 }
+
+resource "azurerm_mssql_database" "sqldb" {
+  name      = "sqldb-sensitive"
+  server_id = azurerm_mssql_server.sqlsrv.id
+  sku_name  = "S0"
 }
-resource "azurerm_sql_server" "sqlsrv" {
-name = "sql-demo-server"
-resource_group_name = azurerm_resource_group.rg.name
-location = azurerm_resource_group.rg.location
-version = "12.0"
-administrator_login = "sqladmin"
-administrator_login_password = "SqlAdminPwd!!"
-}
-resource "azurerm_sql_database" "sqldb" {
-name = "sqldb-sensitive"
-resource_group_name = azurerm_resource_group.rg.name
-server_name = azurerm_sql_server.sqlsrv.name
-sku_name = "S0"
-}
+
+# -----------------------
+# Storage Account
+# -----------------------
 resource "azurerm_storage_account" "storage" {
-name = "stgsecretdemo001"
-resource_group_name = azurerm_resource_group.rg.name
-location = azurerm_resource_group.rg.location
-account_tier = "Standard"
-account_replication_type = "LRS"
-allow_blob_public_access = false
+  name                     = "stgsecretdemo${random_string.rand.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
+
+resource "random_string" "rand" {
+  length  = 4
+  upper   = false
+  special = false
+}
+
+# -----------------------
+# Output
+# -----------------------
 output "storage_connection_string" {
-value = azurerm_storage_account.storage.primary_connection_string
-description = "Connection string"
+  value       = azurerm_storage_account.storage.primary_connection_string
+  sensitive   = true
+  description = "Connection string"
 }
